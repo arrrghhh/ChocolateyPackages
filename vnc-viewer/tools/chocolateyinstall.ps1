39e43f13e520a42e278c6f54e2b7f27bab646c08
ScottMonolith <arrrghhh@gmail.com>
2025-01-13 14:27:28 -0700

    Pull from old repo
    Pulled from old repo - https://github.com/bcurran3/ChocolateyPackages/tree/35fb2b40baa3ed7b672c9f5e84f33888d29e14ba/vnc-viewer

---
+++ vnc-viewer/tools/chocolateyinstall.ps1
@@ -0,0 +1,46 @@
+﻿# https://www.realvnc.com/en/connect/download/viewer/
+$ErrorActionPreference = 'Stop'
+$packageName    = 'vnc-viewer'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$bits           = Get-ProcessorBits
+$extractDir     = "$toolsDir\extracted"
+$url            = "https://downloads.realvnc.com/download/file/viewer.files/VNC-Viewer-$env:ChocolateyPackageVersion-Windows-msi.zip"
+$checksum       = '6E716F0F3FE19946BCF6E425FB96C4B67C87CF3CFB85B13E03DB1A2122C99679'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $extractDir
+  fileType      = 'ZIP' 
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs 
+
+if ($bits -eq 64)
+   {
+    $Installer = "$extractDir\VNC-Viewer-"+$ENV:packageVersion+"-Windows-en-64bit.msi"
+   } else {
+    $Installer = "$extractDir\VNC-Viewer-"+$ENV:packageVersion+"-Windows-en-32bit.msi"
+   }
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'MSI'
+  file           = $Installer
+  silentArgs     = '/quiet /qn /norestart'
+  validExitCodes = @(0, 3010, 1641)
+  softwareName   = 'VNC *'
+}
+ 
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item $extractDir -Recurse -Force | Out-Null
+# Remove portable version from packages versions 6.18.907 -  6.19.325
+Remove-Item "$toolsDir\VNC-Viewer.exe" -Force -ErrorAction SilentlyContinue | Out-Null
+Remove-Item "$ENV:PUBLIC\Desktop\VNC Viewer.lnk" -Force -ErrorAction SilentlyContinue | Out-Null
+Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\VNC Viewer" -Force -ErrorAction SilentlyContinue | Out-Null
+
+# UPDATE INSTRUCTIONS:
+# Update checksum
