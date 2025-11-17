42f8c3fe44ba874db356825c2180e6ecca1dbcb4
arrrghhh <arrrghhh@gmail.com>
2025-11-17 20:08:25 +1100

    Update chocolateyInstall.ps1
    Modify installer script so it references a local executable instead of FileZilla's servers.

--- filezilla/tools/chocolateyInstall.ps1
+++ filezilla/tools/chocolateyInstall.ps1
@@ -1,24 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.filezilla-project.org/client/FileZilla_3.69.5_win32_sponsored2-setup.exe'
-$url64      = 'https://download.filezilla-project.org/client/FileZilla_3.69.5_win64_sponsored2-setup.exe'
 
 $packageArgs = @{
   packageName   = 'filezilla'
   fileType      = 'EXE'
-  url           = $url
-  url64bit      = $url64
-
+  file          = Get-Item $toolsDir\*_win32-setup.exe
+  file64        = Get-Item $toolsDir\*_win64-setup.exe
   softwareName  = 'Filezilla 3*'
-
-  checksum      = '9A29EA8D9C56B61CADE7EC3CCF1C26CCC6466619867926DD655F13C8027ED8C5'
-  checksumType  = 'sha256'
-  checksum64    = '77BD820EB0532CFC5AD6523E6ABE7C287D2C53AE1C9B8DDC156706F32092D03B'
-  checksumType64= 'sha256'
-
   silentArgs    = "/S"
   validExitCodes= @(0, 1223)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
+Get-ChildItem $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content -Value "" -Path "$_.ignore" }}
