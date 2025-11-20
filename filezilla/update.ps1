92acb3f3942409e9cc8e3526d32986713913d5b2
arrrghhh <arrrghhh@gmail.com>
2025-11-20 14:58:36 -0700

    Update update.ps1
    Cleanup from previous commit

--- filezilla/update.ps1
+++ filezilla/update.ps1
@@ -1,20 +1,5 @@
 ﻿Import-Module Chocolatey-AU
 
-<<<<<<< Updated upstream
-$releases = 'https://filezilla-project.org/download.php?show_all=1'
-
-function global:au_SearchReplace {
-   @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
-        }
-
-        ".\legal\VERIFICATION.txt" = @{
-          "(?i)(\s+x32:).*"            = "`${1} $($releases)"
-          "(?i)(\s+x64:).*"            = "`${1} $($releases)"
-          "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
-          "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
-=======
 $PackageId   = "filezilla"
 $ReleasePage = "https://filezilla-project.org/download.php?show_all=1"
 $ToolsDir    = "$PSScriptRoot\tools"
@@ -229,38 +214,10 @@ function global:au_SearchReplace {
         ".\legal\VERIFICATION.txt" = @{
             "(?i)(checksum32:).*" = "`${1} $($Latest.Checksum32)"
             "(?i)(checksum64:).*" = "`${1} $($Latest.Checksum64)"
->>>>>>> Stashed changes
         }
     }
 }
 
-<<<<<<< Updated upstream
-function global:au_BeforeUpdate { Get-RemoteFiles -Purge -FileNameBase "FileZilla_$($Latest.Version)"}
-
-function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing -UserAgent "Chocolatey" -Headers @{
-        "Accept" = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
-    }
-    $url32 = $download_page.Links | Where-Object href -match "win32\-setup\.exe" | Select-Object -first 1 -expand href
-    $url64 = $download_page.Links | Where-Object href -match "win64\-setup\.exe" | Select-Object -first 1 -expand href
-    $version = $url32 -split '_' | Where-Object { $_ -match '^\d+\.[\d\.]+$' } | Select-Object -first 1
-
-    @{
-        Version  = $version
-        URL64    = $url64
-        URL32    = $url32
-        FileType = "exe"
-        Options = @{
-            Headers = @{
-                "Accept" = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
-                "User-Agent" = "Chocolatey"
-            }
-        }
-    }
-}
-
-update -ChecksumFor none
-=======
 $Driver = Start-SeDriver `
     -Browser "firefox" `
     -State Headless `
@@ -271,4 +228,3 @@ update -ChecksumFor none -NoCheckUrl
 
 $Driver.Quit()
 $Driver.Dispose()
->>>>>>> Stashed changes
