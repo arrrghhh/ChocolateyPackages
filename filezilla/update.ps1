f4cb3ed5033511c9d0f851730bb1e76fc612f58a
Scott Brescia <arrrghhh@gmail.com>
2026-02-19 15:26:39 -0700

    Remove au_SearchReplace as it was causing issues

--- filezilla/update.ps1
+++ filezilla/update.ps1
@@ -171,13 +171,6 @@ function global:au_GetLatest {
     }
 }
 
-function global:au_SearchReplace {
-    @{ 'tools/chocolateyInstall.ps1' = @{
-        "(?i)(^\s*[$]url\s*=\s*)(['""].*['""])"      = "`$1'$($Latest.URL32)'"
-        "(?i)(^\s*[$]checksum\s*=\s*)(['""].*['""])" = "`$1'$($Latest.Checksum32)'"
-        "(?i)(^\s*[$]url64\s*=\s*)(['""].*['""])"    = "`$1'$($Latest.URL64)'"
-        "(?i)(^\s*[$]checksum64\s*=\s*)(['""].*['""])" = "`$1'$($Latest.Checksum64)'"
-    }}
 }
 
 # --- 4. Main Execution ---
