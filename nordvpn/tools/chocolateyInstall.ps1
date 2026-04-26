ea6314cfb3fa4506ec10119669a652fc8f6be775
Scott Brescia <arrrghhh@gmail.com>
2026-04-25 22:18:26 -0600

    Make install silent & fix uninstall

--- nordvpn/tools/chocolateyInstall.ps1
+++ nordvpn/tools/chocolateyInstall.ps1
@@ -8,11 +8,10 @@ $packageArgs = @{
     fileType       = 'exe'
     # AU will keep this URL and checksum current on every update
     url            = 'https://downloads.nordcdn.com/apps/windows/NordVPN/8.1.2.0/NordVPNInstall.exe'
-    checksum       = ''
-    checksumType   = ''
-    # NordVPN uses InnoSetup
-    silentArgs     = '/SILENT /SUPPRESSMSGBOXES /NORESTART /SP- /ALLUSERS' `
-                   + " /LOG=`"$($env:TEMP)\$packageName.$($env:chocolateyPackageVersion).Install.log`""
+    checksum       = '0b34bc0173cbaf396b6dcfd753e6dc0f1201abf0495495bcfb27cd903a27f9f5'
+    checksumType   = 'sha256'
+    # NordVPN uses InnoSetup -- /VERYSILENT fully suppresses UI
+    silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /ALLUSERS'
     validExitCodes = @(0)
     softwareName   = 'NordVPN*'
 }
